c97cae48a75fc69ec6a5b77ac395c9c64af66b07
Moshiur Rahman Amit <moshiur.amit@gmail.com>
2023-02-17 17:43:58 +0600

    add file for video

--- react/firstapp/src/pages/Contact.js
+++ react/firstapp/src/pages/Contact.js
@@ -1,9 +1,20 @@
+//import ReactPlayer from "react-player"; // all media 
+import ReactPlayer from "react-player/youtube";
 function Contact() {
     return (
         <div>
             <h1>Write your contact module</h1>
+            <MyVideo />
         </div>    
     )
 }
+
+const MyVideo = () => {
+    return (
+        <div>
+            <ReactPlayer url='https://www.youtube.com/watch?v=tv1u08KOQmg' />
+        </div>
+    );
+};
 
 export default Contact;
